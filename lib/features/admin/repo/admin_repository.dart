2bf192af90cf5e76738bca167d607438d22963f7
Heknon <development@heknon.com>
2022-04-12 00:21:37 +0300

    added admin repository

--- lib/features/admin/repo/admin_repository.dart
+++ lib/features/admin/repo/admin_repository.dart
@@ -1,3 +1,48 @@
-class AdminRepository {
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:vivity/features/admin/service/admin_service.dart';
+import 'package:vivity/features/business/models/business.dart';
+import 'package:vivity/features/user/errors/user_error.dart';
 
+class AdminRepository {
+  static final AdminRepository _adminRepository = AdminRepository._();
+  final AdminService _adminService = AdminService();
+
+  AdminRepository._();
+
+  factory AdminRepository() => _adminRepository;
+
+  List<Business>? _unapprovedBusinesses;
+
+  Future<List<Business>> getUnapprovedBusinesses({
+    bool update = false,
+    bool getImages = true,
+  }) async {
+    if (_unapprovedBusinesses != null && !update) return _unapprovedBusinesses!;
+
+    AsyncSnapshot<List<Business>> snapshot = await _adminService.getUnapprovedBusinesses(getImages: getImages);
+    if (snapshot.hasError || !snapshot.hasData) {
+      throw UserNoAccessException(response: snapshot.error is Response ? snapshot.error as Response : null);
+    }
+
+    List<Business> business = snapshot.data!;
+    _unapprovedBusinesses = business;
+    return _unapprovedBusinesses!;
+  }
+
+  Future<Business> updateBusinessApprovalStatus({
+    required String businessId,
+    required bool approved,
+    required String note,
+  }) async {
+    AsyncSnapshot<Business> snapshot = await _adminService.updateBusinessApproval(businessId: businessId, approved: approved, note: note);
+    if (snapshot.hasError || !snapshot.hasData) {
+      throw UserNoAccessException(response: snapshot.error is Response ? snapshot.error as Response : null);
+    }
+
+    Business business = snapshot.data!;
+    _unapprovedBusinesses?.removeWhere((element) => element.businessId == business.businessId);
+    _unapprovedBusinesses?.add(business);
+    return business;
+  }
 }
